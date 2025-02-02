fd78a389eb00cf2d6cac0f0e580fff94dc19b513
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-02 14:23:10 +0300

    server

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://127.0.0.1:5500', // Your frontend URL
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true
+  }
+});
+
+app.use(cors({
+  origin: 'http://127.0.0.1:5500', // Your frontend URL
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true
+}));
+
+const users = {};
+
+io.on('connection', socket => {
+  console.log('a user connected');
+  
+  socket.on('new-user', name => {
+    users[socket.id] = name;
+    socket.broadcast.emit('user-connected', name);
+  });
+
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
